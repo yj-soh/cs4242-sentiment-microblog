35a8006e9b04443a3f4043246c71b59bdccb6998
YJSoh <soh.you.jun@gmail.com>
2016-02-08 15:13:51 +0800

    Start parser
    Supports somewhat smart tokenization,
    handles emoticons, stopwords, capitalization,
    repeated characters, lemmatization, and negation.

--- reader.py
+++ reader.py
@@ -2,10 +2,6 @@ import json
 import csv
 
 TWEET_DIR = 'data/tweets/'
-TRAINING = 'data/training.csv'
-DEVELOPMENT = 'data/development.csv'
-TESTING = 'data/testing.csv'
-TWEETS_TO_READ = TRAINING
 
 def __read_json(filename):
     return json.load(open(filename))
@@ -17,7 +13,7 @@ def __read_csv(filename):
         for row in reader:
             yield row
 
-def read():
-    files = (row[2] + '.json' for row in __read_csv(TWEETS_TO_READ))
+def read(csvfile):
+    files = (row[2] + '.json' for row in __read_csv(csvfile))
     jsons = (__read_json(TWEET_DIR + file) for file in files)
     return (json['text'] for json in jsons)
---
+++ tweetparser.py
@@ -0,0 +1,145 @@
+import HTMLParser
+import nltk
+from nltk.corpus import stopwords
+from nltk.stem import WordNetLemmatizer
+import re
+
+import reader
+
+TRAINING = 'data/training.csv'
+DEVELOPMENT = 'data/development.csv'
+TESTING = 'data/testing.csv'
+TWEETS_TO_READ = TRAINING
+
+NEGATION = 'not_'
+
+html_parser = HTMLParser.HTMLParser()
+lemmatizer = WordNetLemmatizer()
+stopwords = map(lambda s:str(s), stopwords.words('english'))
+
+re_str_emoticon = r'''
+    (?:
+      [<>]?
+      [:;=8]                     # eyes
+      [\-o\*\']?                 # optional nose
+      [\)\]\(\[dDpP/\:\}\{@\|\\] # mouth      
+      |
+      [\)\]\(\[dDpP/\:\}\{@\|\\] # mouth
+      [\-o\*\']?                 # optional nose
+      [:;=8]                     # eyes
+      [<>]?
+    )'''
+
+re_str_words = r'''
+    (?:@[\w]+)                     # Usernames
+    |
+    (?:\#+[\w]+[\w\'\-]*[\w]+)     # Hashtags
+    |
+    (?:https?:\/\/(?:www\.|(?!www))[^\s\.]+\.[^\s]{2,}|www\.[^\s]+\.[^\s]{2,}) # URLs
+    |
+    (?:[a-z][a-z'\-_]+[a-z])       # Words with apostrophes or dashes.
+    |
+    (?:[+\-]?\d+[,/.:-]\d+[+\-]?)  # Numbers, including fractions, decimals.
+    |
+    (?:[\w]+)                      # Words without apostrophes or dashes.
+    |
+    (?:\.(?:\s*\.){1,})            # Ellipsis dots. 
+    |
+    (?:\S)                         # Everything else that isn't whitespace.
+    '''
+
+re_str_negation = r'''
+    (?:
+        ^(?:never|no|nothing|nowhere|noone|none|not|
+            havent|hasnt|hadnt|cant|couldnt|shouldnt|
+            wont|wouldnt|dont|doesnt|didnt|isnt|arent|aint
+        )$
+    )
+    |
+    n't'''
+
+re_emoticon = re.compile(re_str_emoticon, re.VERBOSE | re.I | re.UNICODE)
+re_words = re.compile(re_str_emoticon + '|' + re_str_words, \
+                      re.VERBOSE | re.I | re.UNICODE)
+re_repeat_char = re.compile(r'(.)\1+')
+re_negation = re.compile(re_str_negation, re.VERBOSE)
+re_clause_punctuation = re.compile('^[.:;!?]$')
+
+def process_word(word):
+    # if is emoticon
+    if re_emoticon.search(word):
+        return word
+    
+    if word in stopwords:
+        return ''
+
+    word = word.lower()
+    word = re_repeat_char.sub(r'\1\1', word)
+    # contractions
+    # slang
+    try:
+        word = str(lemmatizer.lemmatize(word))
+    except UnicodeDecodeError:
+        pass
+    
+    return word
+
+def negate_range(words, start, end):
+    negation = map(lambda w: NEGATION + w, words[start:end])
+    return words[:start] + negation + words[end:]
+
+def handle_negation(words):
+    negations = []
+    punctuations = []
+    is_negation_next = True
+    
+    # alternates indices between negation and punctuation
+    for idx, word in enumerate(words):
+        if is_negation_next and re_negation.match(word):
+            negations.append(idx + 1)
+            is_negation_next = False
+        if not is_negation_next and re_clause_punctuation.match(word):
+            punctuations.append(idx)
+            is_negation_next = True
+    # negates everything ahead if no punctuation found
+    punctuations.append(len(words))
+    
+    if not negations:
+        return words
+    
+    negation_ranges = zip(negations, punctuations)
+    
+    for negation_range in negation_ranges:
+        start, end = negation_range
+        words = negate_range(words, start, end)
+    
+    return words
+
+def parse_tweets(f):
+    for tweet in reader.read(TWEETS_TO_READ):
+        # markup normalization
+        tweet = html_parser.unescape(tweet)
+        tweet = tweet.encode('utf8')
+        
+        words = re_words.findall(tweet)
+        
+        rtweet = []
+        for word in words:
+            result = process_word(word)
+            if isinstance(result, list):
+                rtweet.extend(result)
+            else:
+                rtweet.append(result)
+        rtweet = filter(None, rtweet) # remove empty strings
+        
+        rtweet = handle_negation(rtweet)
+        # rtweet = remove punctuation?
+        
+        f(rtweet)
+
+if __name__ == '__main__':
+    f = open('out.txt', 'w')
+    def write(text):
+        f.write(str(text) + '\n')
+    parse_tweets(write)
+    f.close()
